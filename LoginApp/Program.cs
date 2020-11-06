8b96bb772d10be7c5897c496743707681c4c9b6b
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2020-11-06 14:38:26 +0000

    initial committ

--- LoginApp/Program.cs
+++ LoginApp/Program.cs
@@ -14,9 +14,11 @@ namespace LoginApp
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmLogin());
+
         }
     }
 }
